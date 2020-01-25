b0271129b5bf1564c5fc8ded1f81edf56bac15a3
Pietro <infocavallinipietro@gmail.com>
2020-01-25 12:29:10 +0100

    Modello Attivita

--- src/app/models/attivita.model.ts
+++ src/app/models/attivita.model.ts
@@ -1,8 +1,24 @@
 import { IDDocument } from './iddocument.model';
 
+
+export enum SettoreAttivita {
+    settoreCorso = 10,
+    settorePrenotazione = 20,
+    settoreTorneo = 30
+}
+
 export class Attivita extends IDDocument {
 
-    
+    DENOMINAZIONE:  string;
+    SETTORE:        SettoreAttivita;
+    IDREFER:        string; //ID Riferimento
+    IDAREA:         string;
+    IDLOCATION:     string;
+    IDCAMPO:        string;
+    DATAORAINIZIO:  Date;
+    DATAORAFINE:    Date;
+    IDSPORT:        string; //Sport - Attività di riferimento
+
     constructor() {
         super();
     }

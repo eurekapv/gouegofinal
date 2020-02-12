24f58142bcc8edbe1d251416090afbed8531a7a0
Pietro <infocavallinipietro@gmail.com>
2020-02-12 10:54:55 +0100

    Model CorsoScheduler

---
+++ src/app/models/corsoscheduler.model.ts
@@ -0,0 +1,12 @@
+import { IDDocument } from './iddocument.model';
+
+export class CorsoScheduler extends IDDocument {
+    IDCORSO: string;
+    IDAREAOPERATIVA: string;
+    IDLOCATION: string;
+    IDCAMPO: string;
+    DATA: Date;
+    VALUEGIORNO: number;
+    
+
+}

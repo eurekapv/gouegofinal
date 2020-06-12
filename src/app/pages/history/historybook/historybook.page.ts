91782d28b45c6717a6f2872fe4cf2ce8b27ca236
Andrea Cuppini <andre96.ac@gmail.com>
2020-06-12 17:23:50 +0200

    finita pagina historybook

--- src/app/pages/history/historybook/historybook.page.ts
+++ src/app/pages/history/historybook/historybook.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Prenotazione } from 'src/app/models/prenotazione.model';
+import { Subscription } from 'rxjs';
+import { PrenotazionePianificazione } from 'src/app/models/prenotazionepianificazione.model';
+import { Campo } from 'src/app/models/campo.model';
 
 @Component({
   selector: 'app-historybook',
@@ -6,6 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historybook.page.scss'],
 })
 export class HistorybookPage implements OnInit {
+
+  activePrenotazione: Prenotazione;
+  subActivePrenotazione: Subscription;
+
+  //Elemento 1 di Pianificazione contenuta in activePrenotazione
+  docPianificazione: PrenotazionePianificazione;
+
+ //Location selezionata
+  selectedLocation: Location;
+  
+  //Campo in versione normale
+  selectedCampo: Campo;
 
   constructor() { }
 

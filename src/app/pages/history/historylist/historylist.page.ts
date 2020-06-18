c4cb14a31cee63ac50e4462eacbbeece4fce4416
Pietro <infocavallinipietro@gmail.com>
2020-06-18 17:39:18 +0200

    Spinner HistoryList

--- src/app/pages/history/historylist/historylist.page.ts
+++ src/app/pages/history/historylist/historylist.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Corso } from 'src/app/models/corso.model';
 import { Prenotazione } from 'src/app/models/prenotazione.model';
 import { PrenotazionePianificazione } from 'src/app/models/prenotazionepianificazione.model';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController } from '@ionic/angular';
 import { Utente } from 'src/app/models/utente.model';
 import { throwError, Subscription } from 'rxjs';
 import { UtentePrenotazione } from 'src/app/models/utenteprenotazione.model';
@@ -22,74 +22,81 @@ export class HistorylistPage implements OnInit {
   subListUtentePrenotazioni: Subscription;
 
   selectedView: string='prenotazioni';
-  listCorsi: Corso[]=[];
+  listUtenteCorsi: Corso[]=[];
 
-
-  //NON UTILIZZARE QUESTE VARIABILI//
-  listPrenotazioni: Prenotazione[]=[];
-  listPrenotazioniPianificazione: PrenotazionePianificazione[]=[]
-  //NON UTILIZZARE LE VARIABILI SOPRA//
+  //Mostra il loading
+  receivedPrenotazioni: boolean;
+  receivedCorsi: boolean;
 
   today = new Date();
 
   constructor(
     private navCtrl:NavController,
     private startService:StartService
+    
   ) { }
 
   ngOnInit() {
-    console.log('Entriamo qui');
+    this.receivedPrenotazioni = false;
+    this.receivedCorsi = false;
+
+    //Richiesta utente attuale
     this.subDocUtente = this.startService.utente
                           .subscribe  (elDocUtente => {
                               this.docUtente = elDocUtente;
                               //Utente arrivato
                               if (this.docUtente) {
-                                //Mi sottoscrivo alla ricezione delle prenotazioni
-                                this.subListUtentePrenotazioni = this.startService.listUtentePrenotazioni.subscribe(
-                                  collPrenotazioni => {
-                                    this.listUtentePrenotazione = collPrenotazioni;
-                                    console.log(this.listUtentePrenotazione);
-                                  }
-                                );
-
+                                //Non ho ancora ricevuto nulla
+                                this.receivedPrenotazioni = false;
+                                //Richiedo le Prenotazioni
                                 this.startService.requestUtentePrenotazioni(this.docUtente.ID);
+                                //Mi sottoscrivo alla ricezione
+                                this.sottoscrizionePrenotazioni();
                               }
                           });
 
-
-
-      //this.initProva(4);
   }
 
+  /**
+   * Esegue la sottoscrizione ai dati Prenotazioni
+   * @param idUtente Utente richiesta
+   */
+  sottoscrizionePrenotazioni() {
+      
+      this.subListUtentePrenotazioni = this.startService.listUtentePrenotazioni
+                                          .subscribe(collPrenotazioni => {
+                                              this.listUtentePrenotazione = collPrenotazioni;                                
+                                              this.receivedPrenotazioni = true;
+                                          }
+      );
+  }
+
+  /**
+   * 
+   * @param value Cambio Visualizzazione
+   */
   onChangeSegment(value)
   {
     this.selectedView=value.detail.value;
   }
 
   
-  initProva(num)
-  {
-    let corsoProva : Corso;
-    let prenotaProva: PrenotazionePianificazione;
-    for(let i=0; i<num; i++)
-    {
-      corsoProva=new Corso();
-      prenotaProva=new PrenotazionePianificazione();
-      corsoProva.ID=String(i);
-      prenotaProva.ID=String(i);
-      this.listCorsi.push(corsoProva);
-      this.listPrenotazioniPianificazione.push(prenotaProva);
-      
-    }
-  }
 
+  /**
+   * E' stato cliccato un corso e si vuole visualizzare i dati di Iscrizione
+   * @param selectedCorso Corso Selezionato
+   */
   onClickCorso(selectedCorso: Corso)
   {
     this.navCtrl.navigateForward(['/','historylist','course',selectedCorso.ID])
 
   }
 
-  onClickPrenotazione(selectedPrenotazione:PrenotazionePianificazione)
+  /**
+   * E' stata selezionata una prenotazione e si vuole la scheda di conferma
+   * @param selectedPrenotazione Prenotazione
+   */
+  onClickPrenotazione(selectedPrenotazione:UtentePrenotazione)
   {
     this.navCtrl.navigateForward(['/','historylist','booking',selectedPrenotazione.ID])
   }

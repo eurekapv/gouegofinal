200026b6d84802325a8573c34dc71d1b45851385
Pietro <infocavallinipietro@gmail.com>
2021-03-31 16:37:20 +0200

    Risolto problema hide toolbar a scatti

--- src/app/shared/directives/hide-header.directive.ts
+++ src/app/shared/directives/hide-header.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { Directive, HostListener, Input, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { DomController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
 
 /**
  * Moves away the header when scrolling down.
@@ -9,39 +10,70 @@ import { DomController } from '@ionic/angular';
 })
 export class HideHeaderDirective implements OnInit {
     @Input('header') header: any;
-
+    
     /**
      * Minimum scroll offset from top in pixels until the animation kicks in.
      */
     @Input('scrollThreshold') scrollThreshold: number;
 
     private lastY = 0;
+    private heightForScroll = -1;
 
-    constructor(private renderer: Renderer2, private domCtrl: DomController) {
+    constructor(private renderer: Renderer2, 
+                private domCtrl: DomController,
+                private el: ElementRef,
+                private platform: Platform) {
         // Doing nothing.
     }
 
     ngOnInit(): void {
         this.header = this.header.el;
         this.domCtrl.write(() => {
-            this.renderer.setStyle(this.header, 'transition', 'margin-top 700ms');
+            this.renderer.setStyle(this.header, 'transition', 'margin-top 500ms');
         });
     }
 
-    @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
-        if (
-            $event.detail.scrollTop > (this.scrollThreshold || 0) &&
-            $event.detail.scrollTop > this.lastY
-        ) {
-            this.domCtrl.write(() => {
-                this.renderer.setStyle(this.header, 'margin-top', `-${this.header.clientHeight}px`);
-            });
-        } else {
-            this.domCtrl.write(() => {
-                this.renderer.setStyle(this.header, 'margin-top', '0');
-            });
-        }
+    getHeightForScrollContent(myEl: ElementRef) {
+        
+        if (this.heightForScroll == -1 || isNaN(this.heightForScroll)) {
 
-        this.lastY = $event.detail.scrollTop;
+            if (myEl && myEl.nativeElement) {
+                let domDivCont = myEl.nativeElement.firstElementChild;
+                if (domDivCont) {
+                    this.heightForScroll = domDivCont.clientHeight - this.platform.height() - this.scrollThreshold;
+                }
+            }
+
+        }
+    }
+
+    @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
+
+            //Faccio calcolare l'altezza utile al calcolo
+            //L'altezza è l'intera altezza del contenuto dello Scroll - Altezza del Canvas della pagina
+            this.getHeightForScrollContent(this.el);
+
+            if (this.heightForScroll != -1 ) {
+                
+                if (
+                    $event.detail.scrollTop > (this.scrollThreshold || 0) &&
+                    $event.detail.scrollTop > this.lastY
+                ) {
+                    this.domCtrl.write(() => {
+                        this.renderer.setStyle(this.header, 'margin-top', `-${this.header.clientHeight}px`);
+                        
+                    });
+                } else if ($event.detail.scrollTop < this.heightForScroll){
+                    this.domCtrl.write(() => {
+                        this.renderer.setStyle(this.header, 'margin-top', '0');
+                        
+                    });
+                }
+        
+                this.lastY = $event.detail.scrollTop;
+            }
+            else {
+                console.warn('Directive Scroll: heightForScroll not valid');
+            }
     }
 }

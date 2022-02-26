a245b98c75c979988f201e22563fd1a742f857bc
Pietro Cavallini (Mac) <infocavallinipietro@gmail.com>
2022-02-26 15:30:53 +0100

    Aggiornamento Capacitor 3

---
+++ capacitor.config.ts
@@ -0,0 +1,45 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    "appId": "com.gouego",
+    "appName": "Open Beach",
+    "bundledWebRuntime": false,
+    "webDir": "www",
+    "backgroundColor": "#ffffffff",
+    "android": {
+        "path": "/opt/android-studio/bin/studio.sh"
+    },
+    "ios": {
+      "cordovaLinkerFlags": [
+        "-ObjC"
+      ]
+    },
+    "plugins": {
+      "SplashScreen": {
+        "launchShowDuration": 0,
+        "launchAutoHide": true,
+        "backgroundColor": "#ffffffff",
+        "androidSplashResourceName": "splash",
+        "androidScaleType": "CENTER_CROP",
+        "showSpinner": false,
+        "splashFullScreen": true,
+        "SplashMaintainAspectRatio": "true",
+        "splashImmersive": true
+      }
+    },
+    "cordova": {
+      "preferences": {
+        "ScrollEnabled": "false",
+        "android-minSdkVersion": "19",
+        "BackupWebStorage": "none",
+        "SplashMaintainAspectRatio": "true",
+        "FadeSplashScreenDuration": "300",
+        "SplashShowOnlyFirstTime": "false",
+        "SplashScreen": "screen",
+        "SplashScreenDelay": "0",
+        "androidScaleType": "CENTER_CROP"
+      }
+    }
+  };
+
+export default config;

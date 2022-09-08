d2b849279c33b5fa580380630f1da3ce2f978a73
Cavallini Pietro <info@cavallinipietro.com>
2022-09-08 21:08:58 +0200

    Nuovo pacchetto OB Android

---
+++ resources/b4fnovara/capacitor.config.ts
@@ -0,0 +1,47 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    "appId": "com.b4fnovara",
+    "appName": "Beach For Fun",
+    "bundledWebRuntime": false,
+    "webDir": "www",
+    "backgroundColor": "#ffffffff",
+    "ios": {
+      "cordovaLinkerFlags": [
+        "-ObjC"
+      ]
+    },
+    "plugins": {
+      "SplashScreen": {
+        "launchShowDuration": 0,
+        "launchAutoHide": true,
+        "backgroundColor": "#ffffffff",
+        "androidSplashResourceName": "splash",
+        "androidScaleType": "CENTER_CROP",
+        "showSpinner": false,
+        "splashFullScreen": true,
+        "SplashMaintainAspectRatio": "true",
+        "splashImmersive": true
+      }
+    },
+    "server": {
+      "allowNavigation": [
+        "gouego.com"
+      ]
+    },
+    "cordova": {
+      "preferences": {
+        "ScrollEnabled": "false",
+        "android-minSdkVersion": "19",
+        "BackupWebStorage": "none",
+        "SplashMaintainAspectRatio": "true",
+        "FadeSplashScreenDuration": "300",
+        "SplashShowOnlyFirstTime": "false",
+        "SplashScreen": "screen",
+        "SplashScreenDelay": "0",
+        "androidScaleType": "CENTER_CROP"
+      }
+    }
+  };
+
+export default config;

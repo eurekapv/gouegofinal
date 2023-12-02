225d88b3f32193087c8ec66c5fed64fe5862f4e5
Pietro Cavallini (Mac) <infocavallinipietro@gmail.com>
2023-12-02 22:07:43 +0100

    Aggiornamenti

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -101,4 +101,55 @@ const config: CapacitorConfig = {
 // };
 //#endregion
 
+
+
+//#region DEMOBEACH
+/*
+const config: CapacitorConfig = {
+  "appId": "com.DEMOBEACH",
+  "appName": "DemoBeach",
+  "webDir": "www",
+  "backgroundColor": "#ffffffff",
+  "android": {
+    "path": "application_build/demobeach/android"
+  },
+  "ios": {
+    "path": "application_build/demobeach/ios",
+    "cordovaLinkerFlags": [
+      "-ObjC"
+    ]
+  },
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 0,
+      "launchAutoHide": true,
+      "backgroundColor": "#ffffffff",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": false,
+      "splashFullScreen": true,
+      "SplashMaintainAspectRatio": "true",
+      "splashImmersive": true
+    }
+  },
+  "server": {
+    "allowNavigation": [
+      "gouego.com"
+    ]
+  },
+  "cordova": {
+    "preferences": {
+      "ScrollEnabled": "false",
+      "android-minSdkVersion": "19",
+      "BackupWebStorage": "none",
+      "SplashMaintainAspectRatio": "true",
+      "FadeSplashScreenDuration": "300",
+      "SplashShowOnlyFirstTime": "false",
+      "SplashScreen": "screen",
+      "SplashScreenDelay": "0",
+      "androidScaleType": "CENTER_CROP"
+    }
+  }
+}; */
+//ß#endregion
 export default config;

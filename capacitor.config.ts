73299a04b5baedb88cc5969d83ecf2b12fba31b5
Pietro Cavallini (Mac) <infocavallinipietro@gmail.com>
2024-08-09 17:37:30 +0200

    Aggiornamento IOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 //Credo sia utile solo la prima volta
 /*
 const config: CapacitorConfig = {
-    "appId": "com.gouego",
+    "appId": "com.gouego.openbeach",
     "appName": "Open Beach",
     "webDir": "www",
     "backgroundColor": "#ffffffff",
@@ -105,54 +105,54 @@ const config: CapacitorConfig = {
 //#endregion
 
 //#region PROGETTO RIEDUCA
-
-// const config: CapacitorConfig = {
-//   "appId": "com.gouego.progettorieduca",
-//   "appName": "Progetto Rieduca",
-//   "webDir": "www",
-//   "backgroundColor": "#ffffffff",
-//   "android": {
-//     "path": "application_build/progettorieduca/android"
-//   },
-//   "ios": {
-//     "path": "application_build/progettorieduca/ios",
-//     "cordovaLinkerFlags": [
-//       "-ObjC"
-//     ]
-//   },
-//   "plugins": {
-//     "SplashScreen": {
-//       "launchShowDuration": 0,
-//       "launchAutoHide": true,
-//       "backgroundColor": "#ffffffff",
-//       "androidSplashResourceName": "splash",
-//       "androidScaleType": "CENTER_CROP",
-//       "showSpinner": false,
-//       "splashFullScreen": true,
-//       "SplashMaintainAspectRatio": "true",
-//       "splashImmersive": true
-//     }
-//   },
-//   "server": {
-//     "allowNavigation": [
-//       "gouego.com"
-//     ]
-//   },
-//   "cordova": {
-//     "preferences": {
-//       "ScrollEnabled": "false",
-//       "android-minSdkVersion": "19",
-//       "BackupWebStorage": "none",
-//       "SplashMaintainAspectRatio": "true",
-//       "FadeSplashScreenDuration": "300",
-//       "SplashShowOnlyFirstTime": "false",
-//       "SplashScreen": "screen",
-//       "SplashScreenDelay": "0",
-//       "androidScaleType": "CENTER_CROP"
-//     }
-//   }
-// };
-
+/*
+const config: CapacitorConfig = {
+  "appId": "com.gouego.progettorieduca",
+  "appName": "Progetto Rieduca",
+  "webDir": "www",
+  "backgroundColor": "#ffffffff",
+  "android": {
+    "path": "application_build/progettorieduca/android"
+  },
+  "ios": {
+    "path": "application_build/progettorieduca/ios",
+    "cordovaLinkerFlags": [
+      "-ObjC"
+    ]
+  },
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 0,
+      "launchAutoHide": true,
+      "backgroundColor": "#ffffffff",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "showSpinner": false,
+      "splashFullScreen": true,
+      "SplashMaintainAspectRatio": "true",
+      "splashImmersive": true
+    }
+  },
+  "server": {
+    "allowNavigation": [
+      "gouego.com"
+    ]
+  },
+  "cordova": {
+    "preferences": {
+      "ScrollEnabled": "false",
+      "android-minSdkVersion": "19",
+      "BackupWebStorage": "none",
+      "SplashMaintainAspectRatio": "true",
+      "FadeSplashScreenDuration": "300",
+      "SplashShowOnlyFirstTime": "false",
+      "SplashScreen": "screen",
+      "SplashScreenDelay": "0",
+      "androidScaleType": "CENTER_CROP"
+    }
+  }
+};
+*/
 
 //#endregion
 

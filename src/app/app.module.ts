c05eea52bb43b5e2b7e55d183d8dd4a2ed2ddb62
Cavallini Pietro <info@cavallinipietro.com>
2024-08-20 15:31:18 +0200

    Aggiornamento Anular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,25 +20,20 @@ registerLocaleData(localeIt,'it');
 //innerHTMLTemplatesEnabled: true Consente di indicare Html in componenti come
 //ion-alert, ion-infinite-scroll-content, ion-loading, ion-refresher-content, and ion-toast
 
-@NgModule({
-    declarations: [AppComponent],
+@NgModule({ declarations: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [BrowserModule,
+    bootstrap: [AppComponent], imports: [BrowserModule,
         FormsModule,
-        HttpClientModule,
         IonicModule.forRoot({
             innerHTMLTemplatesEnabled: true,
             backButtonText: 'Indietro'
         }),
         AppRoutingModule,
-        QRCodeModule,
-    ],
-    providers: [
+        QRCodeModule], providers: [
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
         { provide: LOCALE_ID, useValue: 'it' },
-        CryptoService
-    ],
-    bootstrap: [AppComponent]
-})
+        CryptoService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
 

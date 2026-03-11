108e1de4f2a1c8e45053a05ec05df39ce0d1ab2d
Cavallini Pietro <info@cavallinipietro.com>
2026-03-11 18:09:41 +0100

    Aggiornamento Android OB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   production: false,
   appSignature: packageJson.appSignature,
   version: packageJson.version,
-  releaseDate: '2024-07-08',
+  releaseDate: packageJson.releaseDate,
   options: {
     debugMode: 'full' as DebugMode, //off, minimal, full
     overrideViewConfig: null as ViewConfigs, //Quale layout mostrare (desktop = Layout con menù, mobile = tabs, null = automatico)
